11f09d04020b08ec809dadf61265abbbf6a28227
Nikita Zarutskiy <Aboba410@yandex.ru>
2022-07-12 22:02:14 +0500

    Поправил токен

--- back-end/middlewares/auth.js
+++ back-end/middlewares/auth.js
@@ -1,6 +1,8 @@
 const jwt = require('jsonwebtoken');
 const AuthorizationError = require('../utils/errors/authorization-error');
 
+const { NODE_ENV, JWT_SECRET } = process.env;
+
 // eslint-disable-next-line consistent-return
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
@@ -13,7 +15,7 @@ module.exports = (req, res, next) => {
   let payload;
 
   try {
-    payload = jwt.verify(token, 'some-secret-key');
+    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'mb-secret');
   } catch (err) {
     return next(new AuthorizationError('Необходима авторизация'));
   }
